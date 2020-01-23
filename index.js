3cb848673414004f92709fe59aedb7599219bca3
Joe <joe@MacBook-Pro.local>
2020-01-23 17:45:48 -0500

    Baby costructor: gave all instances

--- index.js
+++ index.js
@@ -91,8 +91,10 @@ function Car(model, milesPerGallon) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  this.name = name,
+  this.age = age,
+  this.favoriteToy = favoriteToy
 }
 
 /* 
